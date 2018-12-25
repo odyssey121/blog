6c9e0c64410077413976c0d0ac69bb83cbfcf64e
lesha Shirnin <odyssey121@mail.ru>
2018-12-25 12:56:09 +0300

    fixed merge problem

--- app/api/comment.py
+++ app/api/comment.py
@@ -1,7 +1,6 @@
 from flask import jsonify, g
 from .. import db
 from ..models import Comment
-
 from . import api
 from . errors import forbidden, bad_request
 
@@ -18,12 +17,8 @@ def approve_comment(id):
 	db.session.add(comment)
 	db.session.commit()
 	return jsonify({'status':'ok'})
-
-<<<<<<< HEAD
+	
 @api.route('/comments/<int:id>', methods = ['DELETE'])
-=======
-@api.route('/comment/<int:id>', methods = ['DELETE'])
->>>>>>> 9c1d010460d3c6af320dd10439e5fb3051faeafa
 def delete_comment(id):
 	comment = Comment.query.get_or_404(id)
 	if comment.article.author != g.current_user and not g.current_user.is_admin:
